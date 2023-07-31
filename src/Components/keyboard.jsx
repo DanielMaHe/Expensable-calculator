f604620307e750339931f250431b3d731c70a2f4
Daniel Martinez <martinezh.dani@gmail.com>
2023-07-30 20:51:45 -0600

    add styles for header

--- src/Components/keyboard.jsx
+++ src/Components/keyboard.jsx
@@ -27,3 +27,45 @@ export function Header({ children, name, color }) {
     </div>
   );
 }
+
+export const HeaderStyle = {
+  iconStyle: `
+  grid-column: 1;
+  grid-row: span 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  margin-left:12px;
+
+  `,
+  titleStyle: `
+grid-column: 2;
+grid-row: 1;
+display: flex;
+align-items: center;
+width: 104px;
+height: 16px;
+font-family: sans-serif;
+font-weight: 400;
+font-size: 12px;
+line-height: 16px;
+margin: 0;
+
+`,
+  nameStyle: `
+grid-column: 2;
+  grid-row: 2;
+  display: flex;
+  align-items: center;
+  width: 104px;
+  height: 24px;
+  font-family: sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  margin: 0;
+
+`,
+};
